658777a6cedeae7990cac41e02982706a7632891
Andrew Duckworth <a.duckworth96@live.com>
2023-05-01 11:07:58 +1200

    3D asset working

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import { motion } from 'framer-motion'
+
+import { styles } from '../styles'
+import { ComputersCanvas } from './canvas'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="relative w-full h-screen mx-auto">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className="text-[#915eff]">Andrew</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            <br className="sm:block hidden" />
+            Sint hic aspernatur architecto cum enim officia illo culpa repellat
+            facilis amet in corporis est quaerat nostrum ipsam accusantium,
+            dolore, eius obcaecati.
+          </p>
+        </div>
+      </div>
+      <ComputersCanvas />
+    </section>
   )
 }
 
