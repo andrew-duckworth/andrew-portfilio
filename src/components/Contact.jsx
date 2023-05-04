8d83b64af83804e3240f7488520eed0ed0aa7890
Andrew Duckworth <a.duckworth96@live.com>
2023-05-04 21:42:58 +1200

    test deploy

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,8 +17,50 @@ const Contact = () => {
 
   const [loading, setLoading] = useState(false)
 
-  const handleChange = (e) => {}
-  const handleSubmit = (e) => {}
+  const handleChange = (e) => {
+    const { name, value } = e.target
+
+    setForm({
+      ...form,
+      [name]: value,
+    })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setLoading(true)
+
+    emailjs
+      .send(
+        'service_aggxeph',
+        'template_791efgb',
+        {
+          from_name: form.float,
+          to_name: 'Andrew',
+          from_email: form.email,
+          to_email: 'a.duckworth96@live.com',
+          message: form.message,
+        },
+        'UIwjLOGZxaNm3HQRJ'
+      )
+      .then(
+        () => {
+          setLoading(false)
+          alert('Thank you. I will get back to you as soon as possible.'),
+            setForm({
+              name: '',
+              email: '',
+              message: '',
+            })
+        },
+        (error) => {
+          setLoading(false)
+
+          console.log(error)
+          alert('Something went wrong.')
+        }
+      )
+  }
 
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
